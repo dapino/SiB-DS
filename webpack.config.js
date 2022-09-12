a74b7307c2a64a61ac873f8fa6ae76309fad611e
Daniel Pino <hola@dapino.co>
2022-09-11 22:03:22 -0500

    library structure

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+process.env.NODE_ENV = 'production';
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'web',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          // Compiles Sass to CSS
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              additionalData: '@use "@/scss/_lib.scss";',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      `...`, // Extend existing minimizers
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
